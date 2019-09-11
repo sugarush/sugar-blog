39213305ee32dbb352ae35bce37fbf47bea0723a
Lucifer Avada <lucifer.avada@gmail.com>
2019-09-11 19:53:46 -0400

    set server to run on specified PORT value

--- server/__main__.py
+++ server/__main__.py
@@ -1,7 +1,9 @@
+import os
+
 import handlers
 import resource
 import seed
 
 from server import server
 
-server.run(host='0.0.0.0', port=80, workers=1)
+server.run(host='0.0.0.0', port=os.getenv('PORT', 8080), workers=1)
